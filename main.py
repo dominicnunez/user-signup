790294906e1bb885b2a8b6d40b3d45dcaa91ece9
Dominic <dominic.nunez@gmail.com>
2018-10-10 17:55:01 -0500

    setup main.py, added html files and stylesheet

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, send_from_directory, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/favicon.ico')
+
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/', methods=["GET", "POST"])
+
+def index():
+    if request.method == "POST":
+        return render_template("hello.html")
+    else:
+        render_template("index.html")
